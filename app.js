4a8b69b2d1219ab67078bb69b3272730f09891e0
DESKTOP-SSC5G51\GAEA-75 <sanjay.p@gaeaglobal.com>
2019-04-11 12:18:57 +0530

    Initial release

---
+++ app.js
@@ -0,0 +1,3 @@
+var localTutor=require('./node-tutorial.js');
+var tut = new localTutor.NodeTutorial();  // Create and save object
+tut.pTutor();
---
+++ node-tutorial.js
@@ -0,0 +1,10 @@
+var Tutor=require('./tutorial.js');
+exports.NodeTutorial=function()
+{
+console.log("Node Tutorial")
+this.pTutor = function ()
+{
+var PTutor=Tutor
+PTutor.tutorial();
+}
+}
